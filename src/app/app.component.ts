f8a33c9d3074f2dc5c9b22c503fcc72927fc02ef
Jose-Valdez7 <157552854+Jose-Valdez7@users.noreply.github.com>
2024-09-22 20:52:59 -0500

    angular
    ejemplo1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
+import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserComponent,ProductComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ejemplo1';
+  author:string= "Jose Valdez";
 }
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './product.component.html',
+  
+})
+export class ProductComponent {
+  id: number=100;
+  name:string='Papitas fritas';
+  price: number=0.40;
+}
